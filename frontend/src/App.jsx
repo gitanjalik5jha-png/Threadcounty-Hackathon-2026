020f2ad5eb5b610b4eaa8038c4fb2c61a1325ff7
gitanjalik5jha-png <gitanjalik5jha@gmail.com>
2026-09-17 20:11:27 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     formData.append('file', file);
 
     try {
-axios.post('https://threadcounty-hackathon-2026-1.onrender.com/upload', formData, { {
+axios.post('https://threadcounty-hackathon-2026-1.onrender.com/upload', formData, {
         headers: { 'Content-Type': 'multipart/form-data' }
       });
       alert("Server Response: " + response.data.message);
